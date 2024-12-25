9577fe643452b7ea38ea8c0f344fb11b20290d65
minhazapon <minhajulabedinapon@gmail.com>
2024-12-25 08:05:11 -0800

    added file

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -20,7 +20,7 @@ const Nav = () => {
     return (
         <div className="  ">
 
-            <div className=" navbar bg-base-100 shadow  ">
+            <div className=" navbar fixed  ">
               <div className=" navbar-start">
                 <div className="dropdown">
                   <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
--- src/home file/Banner.jsx
+++ src/home file/Banner.jsx
@@ -3,8 +3,32 @@
 
 
 const Banner = () => {
+
+
+
     return (
+        <div className=" mb-20 background ">
+
+
+        <div className=" ml-20 mr-20 p-10 ">
+             
+
+        <div className=" mt-28  ">
+        <div className=" flex-col md:flex-row lg:flex-row flex justify-between items-center gap-10 ">
         <div>
+        <p className=" one deep ">One Step<br></br> Closer To Your<br></br> <span className=" text-violet-600 ">Dream Job</span></p>
+        </div>
+        <div>
+        <img className=" h-[400px] w-[400px] rounded-full " src="https://img.freepik.com/free-vector/job-vacancy-background-with-chair_23-2147872789.jpg?t=st=1735142433~exp=1735146033~hmac=58141e588989bdd2efc91700b97c3e3d5ca4982805233f7f414a5ee951cec603&w=740"  alt="" />
+        </div>
+        </div>
+        </div>
+            
+        
+        </div>    
+
+         
+         
             
         </div>
     );
