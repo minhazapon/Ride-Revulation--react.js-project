46119b1b5225bfb071ac64a62984fb96947a13f8
minhazapon <minhajulabedinapon@gmail.com>
2024-12-25 04:54:32 -0800

    added file

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -20,8 +20,8 @@ const Nav = () => {
     return (
         <div className="  ">
 
-            <div className="   navbar bg-base-100 shadow  ">
-              <div className="   navbar-start">
+            <div className=" navbar bg-base-100 shadow  ">
+              <div className=" navbar-start">
                 <div className="dropdown">
                   <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
                     <svg
